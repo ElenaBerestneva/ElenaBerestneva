85d8bbcca57c2d82454708515e9dd94c7330ad76
ElenaBerestneva <berestneva_l@mail.ru>
2017-05-30 15:20:14 +0700

    changes

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-<!DOCTYPE html>
-<title>Hello GitHub</title>
-<body>
-<h2>Hello GitHub</h2>
-<p style="text-indent: 25px;"align="justify"> My first page on GitHub</p>
-</body>
-</html>
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello Git');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
